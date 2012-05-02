14a0a6d7d647657862fd666ba95b078d80131ae4
zzhflyqh <zzhflyqh@gmail.com>
2012-05-02 11:11:44 +0800

    use scons to build system

--- 06_test/shared/ByteBuffer.h
+++ 06_test/shared/ByteBuffer.h
@@ -1,27 +1,11 @@
-/* 
- * Copyright (C) 2005,2006,2007 MaNGOS <http://www.mangosproject.org/>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
 #ifndef _BYTEBUFFER_H
 #define _BYTEBUFFER_H
+#include <stdio.h>
+#include <assert.h>
 
-#include "Common.h"
-#include "Errors.h"
-#include "Log.h"
+#ifndef ASSERT
+#define ASSERT(T) assert(T)
+#endif
 
 class ByteBuffer
 {
@@ -381,7 +365,7 @@ class ByteBuffer
     protected:
         bool PrintPosError(bool add, size_t pos, size_t esize) const
         {
-            sLog.outError("ERROR: Attempt %s in ByteBuffer (pos: %u size: %u) value with size: %u",(add ? "put" : "get"),pos, size(), esize);
+            fprintf(stderr, "ERROR: Attempt %s in ByteBuffer (pos: %u size: %u) value with size: %u",(add ? "put" : "get"),pos, size(), esize);
 
             // assert must fail after function call
             return false;
--- 06_test/shared/WorldPacket.h
+++ 06_test/shared/WorldPacket.h
@@ -1,31 +1,11 @@
-/* 
- * Copyright (C) 2005,2006,2007 MaNGOS <http://www.mangosproject.org/>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
 #ifndef MANGOSSERVER_WORLDPACKET_H
 #define MANGOSSERVER_WORLDPACKET_H
 
-#include "Common.h"
 #include "ByteBuffer.h"
 
 class WorldPacket : public ByteBuffer
 {
     public:
-                                                            // just container for later use
         WorldPacket()                                       : ByteBuffer(0), m_opcode(0)
         {
         }
@@ -35,7 +15,7 @@ class WorldPacket : public ByteBuffer
         {
         }
 
-        void Initialize(uint16 opcode, size_t newres=200)
+        void Init(uint16 opcode, size_t newres=200)
         {
             clear();
             _storage.reserve(newres);
