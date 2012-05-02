4c92d0c042655a23654236f0f3d02fb48b47d47d
zzhflyqh <zzhflyqh@gmail.com>
2012-05-02 11:13:14 +0800

    use scons to build system

---
+++ 06_test/test.cc
@@ -0,0 +1,44 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2012 4399.com, Inc. All Rights Reserved
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * @file test.cc
+ * @author zhangzhihua(zhangzhihua@4399.com)
+ * @date 2012/05/02 10:43:53
+ * @brief 
+ *  
+ **/
+
+#include "WorldPacket.h"
+
+int main( void )
+{
+    WorldPacket pack;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
